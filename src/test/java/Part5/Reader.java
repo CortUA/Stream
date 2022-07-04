930b525c1cc60faacc12a2172cd420f043deaa85
CortUA <104569441+CortUA@users.noreply.github.com>
2022-07-04 21:15:22 +0300

    Готово

--- src/test/java/Part5/Reader.java
+++ src/test/java/Part5/Reader.java
@@ -4,35 +4,96 @@ import java.sql.SQLOutput;
 
 public class Reader {
 
-    int book;
-    int book1;
-    int book2;
-    String fio;
-    int phone;
-    int ticket;
+   private int book;
+   private int book1;
+   private int book2;
+   private String fio;
+   private int phone;
+   private int ticket;
+   private int book3;
 
+    public int getBook() {
+        return book;
+    }
 
+    public void setBook(int book) {
+        this.book = book;
+    }
 
-public void takeBooks(int book, int book1){
-    if ((book + book1) <= 5){
-        System.out.println("Читач взяв "+ book1 + " книжок і тепер у нього загалом "+ (book + book1)+ " книг. Штрафу не передбачено"   );
-    } else if ((book + book1) > 5){
-        System.out.println("У читача "+ fio + "забагато книг - "+(book + book1) + " йому буде видано штраф "+ (book1+book-5)*50+ "гривень");
+    public int getBook1() {
+        return book1;
+    }
+
+    public void setBook1(int book1) {
+        this.book1 = book1;
+    }
+
+    public int getBook2() {
+        return book2;
+    }
+
+    public void setBook2(int book2) {
+        this.book2 = book2;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(int ticket) {
+        this.ticket = ticket;
+    }
+
+    public int getBook3() {
+        return book3;
+    }
+
+    public void setBook3(int book3) {
+        this.book3 = book3;
+    }
+
+    public void takeBooks(){
+    if ((getBook() + getBook1()) <= 5){
+        System.out.println("Читач взяв "+ getBook1() + " книжок і тепер у нього загалом "+ (getBook() + getBook1())+ " книг. Штрафу не передбачено" +
+                "\n Повідомлення відправлено за номером телефону "+ getPhone());
+    } else if ((getBook() + getBook1()) > 5){
+        System.out.println("У читача "+ getFio()+ " з номером квитка "+ getTicket() + " - забагато книг - "+(getBook() + getBook1()) + " йому буде видано штраф "+ (getBook1()+getBook()-5)*50+ "гривень"+
+                "\n Повідомлення відправлено за номером телефону "+ getPhone());
     }else {
         System.out.println("ви ввели невірні данні");
     }
 }
-public void returnBooks(int book, int book2){
-    if((book - book2) < 0){
+public void returnBooks(){
+    if((getBook3() - getBook2()) < 0){
         System.out.println("Неможливо повернути більше ніж отримав");
-    }else if ((book - book2) > 5) {
-        System.out.println("Читач"+ fio +" повертає "+book2+ "книжок. Але у нього забагато книжок на момент повернення, він отримує штраф "+ ((book - 5)*50 +" гривень"+
-                "\n якщо він не поверне зайві книги то на наступний раз буде штраф "+((book - book2)-5)*50+ " гривень"));
+    }else if ((getBook3() - getBook2()) > 5) {
+        System.out.println("Читач "+ getFio() +" повертає "+getBook2()+ " книжок. Але у нього забагато книжок на момент повернення, він отримав штраф "+ ((getBook3() - 5)*50 +" гривень"+
+                "\n якщо він не поверне зайві книги то на наступний раз буде штраф "+((getBook3() - getBook2())-5)*50+ " гривень"+
+                "\n Повідомлення відправлено за номером телефону "+ getPhone()));
 
-        } else if (book > 5) {
-        System.out.println("Наразі заборгованності нема. "+fio+" повернув "+ book2 +" книжки. Але є штраф за зайві книги які взяв в минулому місяці - "+ (book-5)*50+ " гривень");
+        } else if (getBook3() > 5) {
+        System.out.println("Наразі заборгованності нема. "+getFio()+" повернув "+ getBook2() +" книжки і тепер книжок менше 5." +
+                "\n Але є штраф за зайві книги які взяв в минулому місяці - "+ (getBook3()-5)*50+ " гривень"+
+                "\n Повідомлення відправлено за номером телефону "+ getPhone());
     }else {
-        System.out.println("дякуємо що користуєтесь бібліотекою відповідально" + fio);
+        System.out.println("дякуємо що користуєтесь бібліотекою відповідально" + getFio()+
+                "\n Повідомлення відправлено за номером телефону "+ getPhone());
     }
     }
 
@@ -44,13 +105,29 @@ public void returnBooks(int book, int book2){
 
     public static void main(String[] args) {
         Reader pasha = new Reader();
-        pasha.fio = " PASHA ";
-        pasha.takeBooks(4, 5);
-        pasha.returnBooks(9, 2);
+        pasha.setFio("Pasha");
+        pasha.setTicket(865);
+        pasha.setPhone(509896745);
+        pasha.setBook(4);
+        pasha.setBook1(5);
+        pasha.setBook2(2);
+        pasha.setBook3(pasha.getBook1()+ pasha.getBook());
+        pasha.takeBooks();
+        pasha.returnBooks();
 
         Reader ivan = new Reader();
-        ivan.fio = "Ivan";
-        ivan.takeBooks(2,1);
+        ivan.setFio("ivan ivanovich petrof");
+        ivan.setTicket(345);
+        ivan.setBook(4);
+        ivan.setBook1(8);
+        ivan.setBook2(9);
+        ivan.setPhone(505957865);
+        ivan.setBook3(ivan.getBook()+ ivan.getBook1());
+        ivan.takeBooks();
+        ivan.returnBooks();
+
+
+
 
 
 
